e43c73a561389e9b2a54612f79d85ca877715d6e
Abbas khan <Abs.khan2001@gmail.com>
2023-10-11 02:49:54 +0500

    new

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Abbas
+          Abbas  khan
         </p>
         <a
           className="App-link"
