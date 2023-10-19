3ae58d79c3b6abbd745bd4415a2ea341795476d3
Abbas khan <Abs.khan2001@gmail.com>
2023-10-19 23:06:03 +0500

    name change

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Abbas  khan
+          Abbas
         </p>
         <a
           className="App-link"
